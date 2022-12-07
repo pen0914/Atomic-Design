04214fb279a526bd308267c4f7e306b104e8a267
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-12-07 18:15:26 +0900

    Atomic Design Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButtons";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/Searchinput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DEfaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "だいち",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@aaa.com",
-  phone: "080-1342-6578",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>poteto</PrimaryButton>
-        <SecondaryButton>ペンギン</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
