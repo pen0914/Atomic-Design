eac35affe906a5f49fd249f84c444232b47bee2e
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-12-07 20:32:43 +0900

    state.var.1

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,18 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
 
 export const Top = () => {
+  const history = useHistory();
+  const onClickAdmin = () => history.pushState("/users");
+  const onClickGeneral = () => alert("一般");
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
